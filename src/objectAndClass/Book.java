ba64873bcec27d780e9ae44b007dfaae490ba263
Pirania69 <kanashevsky@mail.ru>
2023-03-01 17:20:30 +0300

    ДЗ Методы объектов

--- src/objectAndClass/Book.java
+++ src/objectAndClass/Book.java
@@ -1,7 +1,9 @@
 package objectAndClass;
 
+import java.util.Objects;
+
 public class Book {
-     private String bookName;
+     private  String bookName;
      private Author bookAuthor;
     private int bookYear;
     public Book (String bookName, Author bookAuthor, int bookYear){
@@ -22,5 +24,23 @@ public class Book {
     public void setBookYear(int bookYear){
         this.bookYear = bookYear;
     }
+
+    public String toString(){
+        return " Книга: "+ "'" + this.bookName+ "'," + " Автор: "+ this.bookAuthor + " Год издания: " + this.bookYear;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(bookName, book.bookName) && Objects.equals(bookAuthor, book.bookAuthor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookName, bookAuthor);
+    }
+
 
 }
--- src/objectAndClass/Main.java
+++ src/objectAndClass/Main.java
@@ -2,19 +2,22 @@ package objectAndClass;
 
 public class Main {
     public static void main(String[] args) {
-        Author bochka = new Author("Наталья", "Бочка");
-        Author chudnaya = new Author("Анастасия","Чудная");
-        Book Barin = new Book("Барин", bochka, 2021);
-        Book Sirotka = new Book("Сиротка для графа", chudnaya, 2022);
-        System.out.println("Книга " +Barin.getBookName());
-        System.out.println("Автор " +Barin.getBookAuthor().getAuthorFamilyName());
-        System.out.println("Год публикации " +Barin.getBookYear());
-        System.out.println("Книга " +Sirotka.getBookName());
-        System.out.println("Автор " +Sirotka.getBookAuthor().getAuthorFamilyName());
-        System.out.println("Год публикации " +Sirotka.getBookYear());
-        Sirotka.setBookYear(2023);
-        System.out.println("Книга " +Sirotka.getBookName());
-        System.out.println("Автор " + Sirotka.getBookAuthor().getAuthorName()+" "+Sirotka.getBookAuthor().getAuthorFamilyName());
-        System.out.println("Год публикации " +Sirotka.getBookYear());
+        Author bochka = new Author(" Наталья ", " Бочка ");
+        Author chudnaya = new Author(" Анастасия ","Чудная");
+        Book barin = new Book("Барин", bochka, 2021);
+        Book sirotka = new Book("Сиротка для графа", chudnaya, 2022);
+        System.out.println("Книга " +barin.getBookName());
+        System.out.println("Автор " +barin.getBookAuthor().getAuthorFamilyName());
+        System.out.println("Год публикации " +barin.getBookYear());
+        System.out.println("Книга " +sirotka.getBookName());
+        System.out.println("Автор " +sirotka.getBookAuthor().getAuthorFamilyName());
+        System.out.println("Год публикации " +sirotka.getBookYear());
+        sirotka.setBookYear(2023);
+        System.out.println("Книга " +sirotka.getBookName());
+        System.out.println("Автор " + sirotka.getBookAuthor().getAuthorName()+" "+sirotka.getBookAuthor().getAuthorFamilyName());
+        System.out.println("Год публикации " +sirotka.getBookYear());
+
+        System.out.println(barin);
+        System.out.println(sirotka);
     }
 }
