d82b06625d136570083b44c285a887589ade7a1a
Pirania69 <kanashevsky@mail.ru>
2023-03-03 16:09:02 +0300

    Исправил

--- src/kursovaya/Employee.java
+++ src/kursovaya/Employee.java
@@ -9,38 +9,55 @@ public class Employee {
     private int department;
     private double salary;
 
-    public static int countId=1;
+    public static int countId = 1;
 
 
-    public Employee(int department,String family,String name,String lastName,double salary){
+    public Employee(int department, String family, String name, String lastName, double salary) {
 
-        this.family=family;
-        this.name=name;
-        this.lastName=lastName;
-        this.department=department;
-        this.salary=salary;
-        this.id=countId;
+        this.family = family;
+        this.name = name;
+        this.lastName = lastName;
+        this.department = department;
+        this.salary = salary;
+        this.id = countId;
         countId++;
     }
-        public String  toString(){
-            return "Таб.номер: " +this.id+ " Отдел: "+ this.department + " Фамилия: "
-                    + this.family + " Имя: "+this.name + " Отчество: " +this.lastName
-                    + " Оклад: "+this.salary+" рублей";
-        }
-    public int getId(){
+
+    public String toString() {
+        return "Таб.номер: " + this.id + " Отдел: " + this.department + " Фамилия: "
+                + this.family + " Имя: " + this.name + " Отчество: " + this.lastName
+                + " Оклад: " + this.salary + " рублей";
+    }
+
+    public int getId() {
         return this.id;
     }
-    public String getFamily(){
+
+    public String getFamily() {
         return this.family;
     }
-    public String getName(){return this.name;}
-    public String getLastName(){return this.lastName;}
-    public int getDepartment(){return this.department;}
-    public void setDepartment(int department){
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public int getDepartment() {
+        return this.department;
+    }
+
+    public void setDepartment(int department) {
         this.department = department;
     }
-    public double getSalary(){return this.salary;}
-    public void setSalary(int salary){
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public void setSalary(int salary) {
         this.salary = salary;
     }
 
--- src/kursovaya/Main.java
+++ src/kursovaya/Main.java
@@ -3,61 +3,69 @@ package kursovaya;
 public class Main {
 
     public static Employee[] employees = new Employee[10];
-    static void  getListEmployers(){
-       for (int i = 0; i < employees.length; i++) {
-           System.out.println(employees[i]);
-       }
-    }
-    static double sumSalaryMonth(){
-        double sum=0;
-        for (int i = 0; i < employees.length; i++) {
-            sum = sum + employees[i].getSalary();
-        }
-        return  sum;
-    }
-    static double average(){
-        return (double) sumSalaryMonth()/employees.length;
-    }
-    static double minSalary(){
-        double minSalary=employees[0].getSalary();
-        for (int i = 0; i < employees.length; i++) {
 
-            if(employees[i].getSalary()<minSalary){
+    static void getListEmployers() {
+        for (int i = 0; i < employees.length && employees[i] != null; i++) {
+            System.out.println(employees[i]);
+        }
+    }
+
+    static double sumSalaryMonth() {
+        double sum = 0;
+        for (int i = 0; i < employees.length && employees[i] != null; i++) {
+
+            sum = sum + employees[i].getSalary();
+
+        }
+        return sum;
+    }
+
+    static double average() {
+        return (double) sumSalaryMonth() / employees.length;
+    }
+
+    static double minSalary() {
+        double minSalary = employees[0].getSalary();
+        for (int i = 0; i < employees.length && employees[i] != null; i++) {
+
+            if (employees[i].getSalary() < minSalary) {
                 minSalary = employees[i].getSalary();
             }
 
         }
         return minSalary;
     }
-    static double maxSalary(){
-        double maxSalary=employees[0].getSalary();
-        for (int i = 0; i < employees.length; i++) {
 
-            if(employees[i].getSalary()>maxSalary){
+    static double maxSalary() {
+        double maxSalary = employees[0].getSalary();
+        for (int i = 0; i < employees.length && employees[i] != null; i++) {
+
+            if (employees[i].getSalary() > maxSalary) {
                 maxSalary = employees[i].getSalary();
             }
 
         }
         return maxSalary;
     }
-    static void getFIO(){
-        for (int i = 0; i < employees.length; i++) {
-            System.out.println(employees[i].getFamily()+" "+employees[i].getName()+" "+employees[i].getLastName());
+
+    static void getFIO() {
+        for (int i = 0; i < employees.length && employees[i] != null; i++) {
+            System.out.println(employees[i].getFamily() + " " + employees[i].getName() + " " + employees[i].getLastName());
         }
     }
 
     public static void main(String[] args) {
 
-            employees[0] = new Employee(1, "Ivanov", "Ivan", "Ivanovich", 110_000);
-            employees[1] = new Employee(2, "Vasiliev", "Vasil", "Vasilievich", 80_000);
-            employees[2] = new Employee(3, "Antonov", "Anton", "Antonovich", 65_000);
-            employees[3] = new Employee(4, "Petrov", "Petr", "Petrovich", 65_000);
-            employees[4] = new Employee(5, "Pavlov", "Pavel", "Pavlovich", 65_000);
-            employees[5] = new Employee(5, "Sergeev", "Sergei", "Sergeevich", 45_000);
-            employees[6] = new Employee(5, "Michailov", "Michail", "Michailovich", 45_000);
-            employees[7] = new Employee(5, "Andreev", "Andrey", "Andreevich", 40_000);
-            employees[8] = new Employee(5, "Grigoriev", "Grigoriy", "Grigorievich", 55_000);
-            employees[9] = new Employee(5, "Bogov", "Bog", "Bogovich", 55_000);
+        employees[0] = new Employee(1, "Ivanov", "Ivan", "Ivanovich", 110_000);
+        employees[1] = new Employee(2, "Vasiliev", "Vasil", "Vasilievich", 80_000);
+        employees[2] = new Employee(3, "Antonov", "Anton", "Antonovich", 65_000);
+        employees[3] = new Employee(4, "Petrov", "Petr", "Petrovich", 65_000);
+        employees[4] = new Employee(5, "Pavlov", "Pavel", "Pavlovich", 65_000);
+        employees[5] = new Employee(5, "Sergeev", "Sergei", "Sergeevich", 45_000);
+        employees[6] = new Employee(5, "Michailov", "Michail", "Michailovich", 45_000);
+        employees[7] = new Employee(5, "Andreev", "Andrey", "Andreevich", 40_000);
+        employees[8] = new Employee(5, "Grigoriev", "Grigoriy", "Grigorievich", 55_000);
+        employees[9] = new Employee(5, "Bogov", "Bog", "Bogovich", 55_000);
 
         getListEmployers();
         System.out.println(sumSalaryMonth());
